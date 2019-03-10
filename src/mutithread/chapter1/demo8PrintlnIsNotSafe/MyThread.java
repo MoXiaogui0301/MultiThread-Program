2bebe09ad89e7016dd1ebace7561cb25042cf756
文丽 <wendy@wenlideMacBook-Pro.local>
2019-03-10 19:01:11 +0800

    version 1.0.1

---
+++ src/mutithread/chapter1/demo8PrintlnIsNotSafe/MyThread.java
@@ -0,0 +1,10 @@
+package mutithread.chapter1.demo8PrintlnIsNotSafe;
+
+public class MyThread extends Thread{
+    private int i = 5;
+
+    @Override
+    public void run() {
+        System.out.println("i=" + (i--) + " threadName=" + Thread.currentThread().getName());
+    }
+}
